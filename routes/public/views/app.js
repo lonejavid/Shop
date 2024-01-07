f9efbc442de9b6fcefbcac73120e49d39eedf4ec
lonejavid <lonejavid0739@gmail.com>
2024-01-07 10:52:43 +0530

    shop application with add-to-cart and delete-cart functions added

---
+++ routes/public/views/app.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const express=require('express')
+
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize= require('./util/database')
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+//const cartRoutes=require('./routes/cart',)
+
+const Product=require('./models/product');
+const User=require('./models/user');
+const { name } = require('ejs');
+const Cart = require('./models/cart');
+const CartItem=require('./models/CartItem')
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next)=>{
+    console.log("seach.....")
+    User.findByPk(1).then(user=>{
+        req.user=user;
+        
+        next();
+          }).catch(err=>console.log(err));
+})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
+
+
+
+sequelize.//sync({force:true,onDelete:'CASCADE'})
+sync().then(result=>{
+    return User.findByPk(1)
+    
+}).then(user=>{
+    if(!user){
+       return User.create({name:'Max',email:'lone@getMaxListeners.com'})
+    }
+    return Promise.resolve(user);
+}).then(user=>{
+    //console.log(user);
+     return  user.createCart();
+    
+}).then(cart=>{
+    app.listen(3000)
+
+}).catch(err=>console.log("error occured"))
+
+
---
+++ routes/util/database.js
@@ -0,0 +1,10 @@
+
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('expenses','root','0123.qwe.',{
+    dialect:'mysql',
+    host:'localhost',
+    logging:false
+});
+module.exports=sequelize;
